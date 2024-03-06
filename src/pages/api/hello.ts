d0099d32742b8393066781e131841d8d045a9505
ender <ender@korino.dev>
2024-03-06 21:31:42 +0100

    Fix api hello

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -8,21 +8,15 @@ export default async function handler(
   try {
 
     const ipAddress = req.headers['x-forwarded-for'] as string || req.connection.remoteAddress || 'Unknown';
+    const userAgent = req.headers['user-agent'];
+  
+    const locationData = await (await fetch(`https://ipapi.co/${ipAddress}/json/`)).json();
+    const { network, version, city, region, country_name, in_eu, postal, latitude, longitude, timezone, currency_name, country_calling_code, asn, org } = locationData;
+  
+    const data: string = `Hello ${userAgent} \n\n${ipAddress} (${network} - ${version})\n${country_name}, ${region} (EU: ${in_eu})\n${city} (${postal})\nLA ${latitude} LO ${longitude}\n${timezone} ${currency_name} ${country_calling_code}\n${asn} - ${org}`;
+  
     res.setHeader("Content-Type", "text/plain");
-    res.status(200).send(`${ipAddress}`);
-    
-    // const userAgent = req.headers['user-agent'];
-    
-    // const ipData = await (await fetch('https://api.ipify.org?format=json')).json();
-    // const { ip } = ipData;
-  
-    // const locationData = await (await fetch(`https://ipapi.co/${ip}/json/`)).json();
-    // const { network, version, city, region, country_name, in_eu, postal, latitude, longitude, timezone, currency_name, country_calling_code, asn, org } = locationData;
-  
-    // const data: string = `Hello ${userAgent} \n\n${ip} (${network} - ${version})\n${country_name}, ${region} (EU: ${in_eu})\n${city} (${postal})\nLA ${latitude} LO ${longitude}\n${timezone} ${currency_name} ${country_calling_code}\n${asn} - ${org}`;
-  
-    // res.setHeader("Content-Type", "text/plain");
-    // res.status(200).send(data);
+    res.status(200).send(data);
   } catch (error) {
     console.error("Error fetching data:", error);
     res.status(500).send("error")
