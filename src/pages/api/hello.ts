209463c01c2bb70c6823dc8bf761bc571ad04e89
ender <ender@korino.dev>
2024-03-06 21:24:33 +0100

    Add more user information to hello

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,25 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 
-type ResponseData = {
-  message: string;
-};
-
-export default function handler(
+// prettier-ignore
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
+  res: NextApiResponse<string>
 ) {
-  const secretKey = "123456789";
-  res.status(200).json({ message: "wsg" });
+  try {
+    const userAgent = req.headers['user-agent'];
+    
+    const ipData = await (await fetch('https://api.ipify.org?format=json')).json();
+    const { ip } = ipData;
+  
+    const locationData = await (await fetch(`https://ipapi.co/${ip}/json/`)).json();
+    const { network, version, city, region, country_name, in_eu, postal, latitude, longitude, timezone, currency_name, country_calling_code, asn, org } = locationData;
+  
+    const data: string = `Hello ${userAgent} \n\n${ip} (${network} - ${version})\n${country_name}, ${region} (EU: ${in_eu})\n${city} (${postal})\nLA ${latitude} LO ${longitude}\n${timezone} ${currency_name} ${country_calling_code}\n${asn} - ${org}`;
+  
+    res.setHeader("Content-Type", "text/plain");
+    res.status(200).send(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).send("error")
+  }
 }
