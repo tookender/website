a22769888bfd24599297a6f40a3134e332017b2b
ender <ender@korino.dev>
2024-03-06 20:59:00 +0100

    secret key for testing

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string;
+type ResponseData = {
+  message: string;
 };
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<ResponseData>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const secretKey = "123456789";
+  res.status(200).json({ message: "wsg" });
 }
