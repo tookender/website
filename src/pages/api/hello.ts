0c93078cd03ae82a00913789244fe19787dadab0
ender <ender@korino.dev>
2024-03-06 21:53:55 +0100

    add console log text

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -13,13 +13,18 @@ export default async function handler(
     // const locationData = await (await fetch(`https://ipapi.co/${ipAddress}/json/`)).json();
     // const { network, version, city, region, country_name, in_eu, postal, latitude, longitude, timezone, currency_name, country_calling_code, asn, org } = locationData;#
     // const data: string = `Hello ${userAgent} \n\n${ipAddress} (${network} - ${version})\n${country_name}, ${region} (EU: ${in_eu})\n${city} (${postal})\nLA ${latitude} LO ${longitude}\n${timezone} ${currency_name} ${country_calling_code}\n${asn} - ${org}`;
-
   
     // Code for IP-API.com (45 requests per minute)
     const locationData = await (await fetch(`http://ip-api.com/json/${ipAddress}?fields=status,message,continent,country,regionName,city,zip,lat,lon,timezone,offset,currency,isp,org,as,reverse,mobile,proxy,hosting,query`)).json();
     const { country, city, continent, regionName, zip, lat, lon, timezone, currency, as, org, reverse, mobile, proxy, hosting, isp } = locationData;
     const data: string = `Hello ${userAgent} \n\n${ipAddress}\n${continent}, ${country}, ${regionName}\n${city} (${zip})\nLAT ${lat} LON ${lon}\n${timezone} ${currency}\n${as} - ${org} (${isp})\n${reverse}\n\nMOBILE: ${mobile}\nPROXY: ${proxy}\nHOSTING: ${hosting}`;
   
+    console.log("");
+    console.log("👋 hi there, just a fun tool to view info about your header requests & ip");
+    console.log("⚠️ info might not always be accurate, especially latitude, longitude, and all the booleans (mobile, proxy, hosting)");
+    console.log("📜 don't worry this isn't logged, view the code at: https://github.com/tookender/website/blob/main/src/pages/api/hello.ts");
+    console.log("");
+
     res.setHeader("Content-Type", "text/plain");
     res.status(200).send(data);
   } catch (error) {
