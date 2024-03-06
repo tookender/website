346b733cd6dce819bfe5a219a77161e56656464f
ender <ender@korino.dev>
2024-03-06 21:27:45 +0100

    Testing something

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -6,18 +6,22 @@ export default async function handler(
   res: NextApiResponse<string>
 ) {
   try {
-    const userAgent = req.headers['user-agent'];
+
+    const ipAddress = req.headers['x-forwarded-for'] as string || req.connection.remoteAddress;
+    res.status(200).json({ ip: ipAddress });
     
-    const ipData = await (await fetch('https://api.ipify.org?format=json')).json();
-    const { ip } = ipData;
+    // const userAgent = req.headers['user-agent'];
+    
+    // const ipData = await (await fetch('https://api.ipify.org?format=json')).json();
+    // const { ip } = ipData;
   
-    const locationData = await (await fetch(`https://ipapi.co/${ip}/json/`)).json();
-    const { network, version, city, region, country_name, in_eu, postal, latitude, longitude, timezone, currency_name, country_calling_code, asn, org } = locationData;
+    // const locationData = await (await fetch(`https://ipapi.co/${ip}/json/`)).json();
+    // const { network, version, city, region, country_name, in_eu, postal, latitude, longitude, timezone, currency_name, country_calling_code, asn, org } = locationData;
   
-    const data: string = `Hello ${userAgent} \n\n${ip} (${network} - ${version})\n${country_name}, ${region} (EU: ${in_eu})\n${city} (${postal})\nLA ${latitude} LO ${longitude}\n${timezone} ${currency_name} ${country_calling_code}\n${asn} - ${org}`;
+    // const data: string = `Hello ${userAgent} \n\n${ip} (${network} - ${version})\n${country_name}, ${region} (EU: ${in_eu})\n${city} (${postal})\nLA ${latitude} LO ${longitude}\n${timezone} ${currency_name} ${country_calling_code}\n${asn} - ${org}`;
   
-    res.setHeader("Content-Type", "text/plain");
-    res.status(200).send(data);
+    // res.setHeader("Content-Type", "text/plain");
+    // res.status(200).send(data);
   } catch (error) {
     console.error("Error fetching data:", error);
     res.status(500).send("error")
