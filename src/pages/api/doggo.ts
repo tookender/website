cd49b32ff8f89bc3b82bda349450ff14ba60821f
ender <ender@korino.dev>
2024-03-07 18:51:51 +0100

    add semicolon

--- src/pages/api/doggo.ts
+++ src/pages/api/doggo.ts
@@ -10,7 +10,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  const data = getRandomDog(true)
+  const data = getRandomDog(true);
 
   res.status(200).json({ url: data[0], description: data[1] });
 }
