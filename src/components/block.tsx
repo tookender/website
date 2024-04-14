38637680faecf047255cf487f7fa983bacb370df
ender <ender@korino.dev>
2024-04-14 13:19:56 +0200

    😎 Add cool hover effect to block

--- src/components/block.tsx
+++ src/components/block.tsx
@@ -12,11 +12,11 @@ export const Block = ({ text, link, image, alt }: BlockProps) => {
   return (
     <a
       href={link}
-      className="inline-flex items-center rounded border border-neutral-700 bg-neutral-800 p-1 text-sm leading-4 text-neutral-100 no-underline duration-500 hover:border-neutral-500 hover:bg-neutral-700"
+      className="inline-flex items-center rounded border border-neutral-700 bg-neutral-800 p-1 text-sm leading-4 text-neutral-100 no-underline duration-300 hover:border-neutral-500 hover:bg-neutral-700 active:scale-95"
     >
       <Image
         src={image}
-        className="!mr-1 rounded-full"
+        className="mr-1 rounded-full"
         width={14}
         height={14}
         alt={alt}
