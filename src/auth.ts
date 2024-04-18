0e8df25bb2e2ccdb6c261671dec95b04ed9b49db
ender <ender@korino.dev>
2024-04-18 15:51:01 +0200

    💡 Update packages and add MongoDB

--- src/auth.ts
+++ src/auth.ts
@@ -1,19 +1,9 @@
 import NextAuth from "next-auth"
-import PostgresAdapter from "@auth/pg-adapter"
-import { Pool } from "pg"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "@/lib/db"
 import GitHub from "next-auth/providers/github"
  
-const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-})
- 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PostgresAdapter(pool),
+  adapter: MongoDBAdapter(clientPromise),
   providers: [GitHub],
 })
