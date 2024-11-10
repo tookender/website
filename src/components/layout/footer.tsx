b93f7a418dcdcc27b74ff7ea294aa535002d8a82
ender <68495763+tookender@users.noreply.github.com>
2024-11-10 17:44:52 +0100

    🌐 try new git commit SHA hash in footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,13 +1,22 @@
+import { FiGitCommit } from "react-icons/fi";
+
 export const Footer = () => {
+  const gitCommit = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ? process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA.substring(0, 7) : "Development Version";
+
   return (
     <footer className="flex flex-col items-start justify-center border-t-2 border-neutral-700 mt-12">
-			<h1 className="mt-4 ml-4 text-2xl font-semibold text-neutral-400/80">
-				tookender
-			</h1>
+      <h1 className="mt-4 ml-4 text-2xl font-semibold text-neutral-400/80">
+        tookender
+      </h1>
 
-			<p className="text-neutral-300/80 ml-4">
-				Full-stack developer • {new Date().getFullYear()}
-			</p>
+      <p className="text-neutral-300/80 ml-4">
+        Full-stack developer • {new Date().getFullYear()}
+      </p>
+
+      <div className="flex flex-row text-neutral-300/80 ml-4 mt-1 gap-1">
+        <FiGitCommit/> 
+        <p>{gitCommit}</p>
+      </div>
     </footer>
   );
 };
