b51787289050025ef30ece2836f5e8b666079bb8
ender <68495763+tookender@users.noreply.github.com>
2025-02-17 07:17:05 +0100

    📍 Update ipinfo API to get client's IP

--- src/app/api/location/route.ts
+++ src/app/api/location/route.ts
@@ -1,5 +1,8 @@
-export async function GET() {
-  const res = await fetch("https://ipinfo.io/json?token=c34d1c9b484916", {
+import { NextRequest } from "next/server"
+
+export async function GET(req: NextRequest) {
+  const ip = (req.headers.get("x-forwarded-for") ?? "127.0.0.1").split(',')[0]
+  const res = await fetch(`https://ipinfo.io/${ip}/json?token=c34d1c9b484916`, {
     headers: {
       "Content-Type": "application/json",
     }
